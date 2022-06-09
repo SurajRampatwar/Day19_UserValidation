56cbf0a9302b334edc11ba509a6f816edc3c93f9
SurajRampatwar <surajrampatwar123@gmail.com>
2022-06-09 22:57:58 +0530

    ADDED:Email Id and Mobile Number

--- src/UserValidation.java
+++ src/UserValidation.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class UserValidation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -9,11 +10,19 @@ public class UserValidation {
         System.out.println("Enter Last Name");
         String lastName = scan.next();
         String regex = "^[A-Z][a-z]{3,}$";
-        Pattern p = Pattern.compile(regex);
-        boolean fName = firstName.matches(p.pattern());
-        boolean lName = lastName.matches(p.pattern());
+        System.out.println("Enter Email Id");
+        String emailId = scan.next();
+        String regex1 = "^abc(.+)[a-z]*@bl(.)co(.)+[a-z]$";
+        System.out.println("Enter Mobile Number");
+        String mobileNo = scan.next();
+        String regex2 = "^[0-9]{2}+[6-9][0-9]{9}";
+        boolean fName = firstName.matches(regex);
+        boolean lName = lastName.matches(regex);
+        boolean email = emailId.matches(regex1);
+        boolean mobile = mobileNo.matches(regex2);
         System.out.println(fName);
         System.out.println(lName);
-
+        System.out.println(email);
+        System.out.println(mobile);
     }
 }
