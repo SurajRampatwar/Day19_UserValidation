7c41d6b35a915824ddff4b2d80b550032a665fa2
SurajRampatwar <surajrampatwar123@gmail.com>
2022-06-09 17:12:31 +0530

    ADDED:Last Name

--- src/UserValidation.java
+++ src/UserValidation.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 public class UserValidation {
     public static void main(String[] args) {
-        System.out.println("Enter First name");
         Scanner scan = new Scanner(System.in);
-        String name = scan.next();
+        System.out.println("Enter First Name");
+        String firstName = scan.next();
+        System.out.println("Enter Last Name");
+        String lastName = scan.next();
         String regex = "^[A-Z][a-z]{3,}$";
-
         Pattern p = Pattern.compile(regex);
-        Matcher m = p.matcher(name);
-        boolean result = name.matches(p.pattern());
-        System.out.println(result);
+        boolean fName = firstName.matches(p.pattern());
+        boolean lName = lastName.matches(p.pattern());
+        System.out.println(fName);
+        System.out.println(lName);
 
     }
 }
